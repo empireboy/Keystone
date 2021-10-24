20e9b0bd54edd4eb50de4c46f21ad783de05311e
Casper Meloen <casper.meloen@hva.nl>
2021-10-25 01:09:34 +0200

    Added AutoStartPlayerTurn which automatically starts the players turn from the second turn and onwards. Also added a visual slider

---
+++ Assets/Scripts/Game Managers/Turn Manager/AutoStartPlayerTurnSlider.cs
@@ -0,0 +1,50 @@
+using CM.Events;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class AutoStartPlayerTurnSlider : MonoBehaviour
+{
+	[SerializeField]
+	private Slider _slider;
+
+	private float _automationTime;
+	private float _currentAutomationTime;
+
+	private void Awake()
+	{
+		EventManager.AddListener<TurnEndEvent>(OnTurnEnd);
+
+		FindObjectOfType<AutoStartPlayerTurn>().OnStartAutomation += OnStartAutomation;
+
+		_slider.gameObject.SetActive(false);
+	}
+
+	private void Update()
+	{
+		if (!_slider.gameObject.activeInHierarchy)
+			return;
+
+		_currentAutomationTime -= Time.deltaTime;
+		_slider.value = Mathf.Lerp(0, 1, _currentAutomationTime / _automationTime);
+	}
+
+	private void OnStartAutomation(float time)
+	{
+		_automationTime = time;
+		_slider.value = 1;
+		_currentAutomationTime = _automationTime;
+
+		_slider.gameObject.SetActive(true);
+	}
+
+	private void OnTurnEnd(object eventData)
+	{
+		TurnEndEvent turnEndEvent = eventData as TurnEndEvent;
+
+		if (turnEndEvent.Turn != TurnManager.TurnStates.PlayerTurn)
+			return;
+
+		_slider.gameObject.SetActive(false);
+	}
+}
